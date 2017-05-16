a0975df150a0f24c7ade9a229f138436caa955ed
senlinzhan <senlinzhan@gmail.com>
2017-05-16 23:31:58 +0800

    adjust style

--- requests/Context.cpp
+++ requests/Context.cpp
@@ -10,34 +10,34 @@ Context::Context(IOService &service, const Url &url, Method method, const String
 }        
     
 Context::Context(IOService &service, const Url &url, Method method, const StringMap &data, const UserCallback &callback)
-        : sock_(service),
-          url_(url),
-          callback_(callback),
-          method_(method)
+    : sock_(service),
+      url_(url),
+      callback_(callback),
+      method_(method)
+{
+    std::ostream reqStream(&requestBuff_);
+    
+    if (method_ == Method::Get)           
     {
-        std::ostream reqStream(&requestBuff_);
-
-        if (method_ == Method::Get)           
-        {
-            url_.addQueries(data);
-            
-            reqStream << "GET " << url_.pathAndQueries() << " HTTP/1.1\r\n";
-            reqStream << "Host: " << url_.host() << "\r\n";
-            reqStream << "Accept: */*\r\n";
-            reqStream << "Connection: close\r\n\r\n";                        
-        }
-        else if (method_ == Method::Post)
-        {
-            auto requestBody = urlEncode(data);
-            auto length = std::to_string(requestBody.size());
-            
-            reqStream << "POST " << url_.path() << " HTTP/1.1\r\n";
-            reqStream << "Host: " << url_.host() << "\r\n";
-            reqStream << "Accept: */*\r\n";
-            reqStream << "Content-Type: application/x-www-form-urlencoded\r\n";
-            reqStream << "Content-Length: " << length << "\r\n";
-            reqStream << "Connection: close\r\n\r\n";
-
-            reqStream << requestBody;
-        }
+        url_.addQueries(data);
+        
+        reqStream << "GET " << url_.pathAndQueries() << " HTTP/1.1\r\n";
+        reqStream << "Host: " << url_.host() << "\r\n";
+        reqStream << "Accept: */*\r\n";
+        reqStream << "Connection: close\r\n\r\n";                        
     }
+    else if (method_ == Method::Post)
+    {
+        auto requestBody = urlEncode(data);
+        auto length = std::to_string(requestBody.size());
+        
+        reqStream << "POST " << url_.path() << " HTTP/1.1\r\n";
+        reqStream << "Host: " << url_.host() << "\r\n";
+        reqStream << "Accept: */*\r\n";
+        reqStream << "Content-Type: application/x-www-form-urlencoded\r\n";
+        reqStream << "Content-Length: " << length << "\r\n";
+        reqStream << "Connection: close\r\n\r\n";
+        
+        reqStream << requestBody;
+    }
+}
